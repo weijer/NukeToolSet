72c0d70fbc96da8439bc63a94ce42a7b5bf9c45c
chengwei <weiwei163@foxmai.com>
2025-08-25 14:20:57 +0800

    fixed: 默认浮点exr import 为 srgb色彩空间

--- python/ToolSet/set_srgb_colorspace.py
+++ python/ToolSet/set_srgb_colorspace.py
@@ -4,5 +4,5 @@ import nuke
 def main():
     nuke.root()["colorManagement"].setValue("Nuke")  # 启用Nuke默认模式
     nuke.root()["OCIO_config"].setValue(2)  # 1表示使用自定义路径
-    nuke.root()["floatLut"].setValue("sRGB") # 默认浮点exr import 为 srgb色彩空间
+    nuke.root()["floatLut"].setValue("sRGB")  # 默认浮点exr import 为 srgb色彩空间
     nuke.root()["customOCIOConfigPath"].setValue("")  # 设置路径
